1af859225c7c7db25b0b865e6f7963b968aad778
gruuman <sale2022@gmail.com>
2020-04-02 19:01:32 +0200

    Made some changes to the main app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,11 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'Hello World' ;
-  }
-  @Get('world')
-  getWorld(): string {
-    return 'World!!!' ;
+  getIndex(): string {
+    return 'Home page!' ;
   }
 }
